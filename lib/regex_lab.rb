9bc175676b362e20904642292660bec23b748ec7
Ethan <ethanchristianwayne@gmail.com>
2020-11-03 00:21:31 -0500

    commit tree 20cd49ca484e6d00239ff3d477c0fff6fda5d7a5

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,26 @@
-def starts_with_a_vowel?(word)
-
+def starts_with_vowel?(word)
+    if word.match(/\A[aeiouAEIOU]\w+/)
+        return true
+    else
+        return false
+    end
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
-
+def
+    words_starting_with_un_and_ending_with_ing(text)
+    return
+    text.scan(/[Un|Un]\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+    return text.scan(/\b[a-zA-Z]{5}\b/)
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
-
+def
+    first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
 
+    phone.match(/\D*\d{3}\D*\d{3}\D*\d{4}/)
 end
